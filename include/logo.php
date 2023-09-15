decab9a969c15155f3173be3359b4d8ebcc59711
Kirstirst <irinapanina2323@mail.ru>
2023-09-15 15:06:50 +0300

    task150358

---
+++ include/logo.php
@@ -0,0 +1,2 @@
+<a href="<?=SITE_DIR?>" class="h5 text-uppercase text-black"><strong>HomeSpace<span
+            class="text-danger">.</span></strong></a>
--- local/templates/home/footer.php
+++ local/templates/home/footer.php
@@ -1,16 +1,23 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+IncludeTemplateLangFile(__FILE__);
+?>
 <footer class="site-footer">
     <div class="container">
         <div class="row">
             <div class="col-lg-4">
                 <div class="mb-5">
-                    <h3 class="footer-heading mb-4"><?=GetMessage("ABOUT_HOMESPACE")?></h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe pariatur reprehenderit vero atque,
-                        consequatur id ratione, et non dignissimos culpa? Ut veritatis, quos illum totam quis blanditiis, minima
-                        minus odio!</p>
-                </div>
-
-
-
+                    <?$APPLICATION->IncludeComponent(
+                        "bitrix:main.include",
+                        "",
+                        Array(
+                            "AREA_FILE_SHOW" => "file",
+                            "AREA_FILE_SUFFIX" => "inc",
+                            "EDIT_TEMPLATE" => "",
+                            "PATH" => "/include/space.php"
+                        )
+                    );?>
+                    </div>
             </div>
             <div class="col-lg-4 mb-5 mb-lg-0">
                 <div class="row mb-5">
@@ -34,40 +41,53 @@
                         </ul>
                     </div>
                 </div>
-
-
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:menu",
+                    ".default",
+                    array(
+                        "ALLOW_MULTI_SELECT" => "N",
+                        "CHILD_MENU_TYPE" => "left",
+                        "DELAY" => "N",
+                        "MAX_LEVEL" => "1",
+                        "MENU_CACHE_GET_VARS" => array(
+                        ),
+                        "MENU_CACHE_TIME" => "3600",
+                        "MENU_CACHE_TYPE" => "N",
+                        "MENU_CACHE_USE_GROUPS" => "Y",
+                        "ROOT_MENU_TYPE" => "top",
+                        "USE_EXT" => "N",
+                        "COMPONENT_TEMPLATE" => ".default"
+                    ),
+                    false
+                );?>
             </div>
-
             <div class="col-lg-4 mb-5 mb-lg-0">
-                <h3 class="footer-heading mb-4">Follow Us</h3>
-
-                <div>
-                    <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
-                    <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
-                    <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
-                    <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:main.include",
+                    "",
+                    Array(
+                        "AREA_FILE_SHOW" => "file",
+                        "AREA_FILE_SUFFIX" => "inc",
+                        "EDIT_TEMPLATE" => "",
+                        "PATH" => "/include/subs.php"
+                    )
+                );?>
                 </div>
-
-
-
             </div>
-
         </div>
         <div class="row pt-5 mt-5 text-center">
-            <div class="col-md-12">
-                <p>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    Copyright &copy;</script>
-                    <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with
-                    <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com"
-                                                                                    target="_blank">Colorlib</a>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                </p>
-            </div>
+            <?$APPLICATION->IncludeComponent(
+                "bitrix:main.include",
+                "",
+                Array(
+                    "AREA_FILE_SHOW" => "file",
+                    "AREA_FILE_SUFFIX" => "inc",
+                    "EDIT_TEMPLATE" => "",
+                    "PATH" => "/include/right.php"
+                )
+            );?>
 
         </div>
-    </div>
 </footer>
 </body>
-
 </html>
