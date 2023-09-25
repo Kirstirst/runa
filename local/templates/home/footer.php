a0f351db34e0d10a4c465181ce9e16305e8018fd
Kirstirst <irinapanina2323@mail.ru>
2023-09-25 22:20:27 +0300

    task150359

--- local/templates/home/footer.php
+++ local/templates/home/footer.php
@@ -1,4 +1,4 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 <?
 IncludeTemplateLangFile(__FILE__);
 ?>
@@ -7,87 +7,62 @@ IncludeTemplateLangFile(__FILE__);
         <div class="row">
             <div class="col-lg-4">
                 <div class="mb-5">
-                    <?$APPLICATION->IncludeComponent(
+                    <? $APPLICATION->IncludeComponent(
                         "bitrix:main.include",
                         "",
-                        Array(
+                        array(
                             "AREA_FILE_SHOW" => "file",
                             "AREA_FILE_SUFFIX" => "inc",
                             "EDIT_TEMPLATE" => "",
                             "PATH" => "/include/space.php"
                         )
-                    );?>
-                    </div>
-            </div>
-            <div class="col-lg-4 mb-5 mb-lg-0">
-                <div class="row mb-5">
-                    <div class="col-md-12">
-                        <h3 class="footer-heading mb-4">Navigations</h3>
-                    </div>
-                    <div class="col-md-6 col-lg-6">
-                        <ul class="list-unstyled">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Buy</a></li>
-                            <li><a href="#">Rent</a></li>
-                            <li><a href="#">Properties</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-6 col-lg-6">
-                        <ul class="list-unstyled">
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                            <li><a href="#">Terms</a></li>
-                        </ul>
-                    </div>
+                    ); ?>
                 </div>
-                <?$APPLICATION->IncludeComponent(
-                    "bitrix:menu",
-                    ".default",
-                    array(
-                        "ALLOW_MULTI_SELECT" => "N",
-                        "CHILD_MENU_TYPE" => "left",
-                        "DELAY" => "N",
-                        "MAX_LEVEL" => "1",
-                        "MENU_CACHE_GET_VARS" => array(
-                        ),
-                        "MENU_CACHE_TIME" => "3600",
-                        "MENU_CACHE_TYPE" => "N",
-                        "MENU_CACHE_USE_GROUPS" => "Y",
-                        "ROOT_MENU_TYPE" => "top",
-                        "USE_EXT" => "N",
-                        "COMPONENT_TEMPLATE" => ".default"
-                    ),
-                    false
-                );?>
             </div>
             <div class="col-lg-4 mb-5 mb-lg-0">
-                <?$APPLICATION->IncludeComponent(
+                <? $APPLICATION->IncludeComponent("bitrix:menu", "bottom_menu", array(
+                    "ALLOW_MULTI_SELECT" => "N",    // Разрешить несколько активных пунктов одновременно
+                    "CHILD_MENU_TYPE" => "left",    // Тип меню для остальных уровней
+                    "DELAY" => "N",    // Откладывать выполнение шаблона меню
+                    "MAX_LEVEL" => "1",    // Уровень вложенности меню
+                    "MENU_CACHE_GET_VARS" => "",    // Значимые переменные запроса
+                    "MENU_CACHE_TIME" => "3600",    // Время кеширования (сек.)
+                    "MENU_CACHE_TYPE" => "N",    // Тип кеширования
+                    "MENU_CACHE_USE_GROUPS" => "Y",    // Учитывать права доступа
+                    "ROOT_MENU_TYPE" => "top",    // Тип меню для первого уровня
+                    "USE_EXT" => "N",    // Подключать файлы с именами вида .тип_меню.menu_ext.php
+                    "COMPONENT_TEMPLATE" => ".default"
+                ),
+                    false
+                ); ?>
+            </div>
+            <div class="col-lg-4 mb-5 mb-lg-0">
+                <? $APPLICATION->IncludeComponent(
                     "bitrix:main.include",
                     "",
-                    Array(
+                    array(
                         "AREA_FILE_SHOW" => "file",
                         "AREA_FILE_SUFFIX" => "inc",
                         "EDIT_TEMPLATE" => "",
                         "PATH" => "/include/subs.php"
                     )
-                );?>
-                </div>
+                ); ?>
             </div>
         </div>
-        <div class="row pt-5 mt-5 text-center">
-            <?$APPLICATION->IncludeComponent(
-                "bitrix:main.include",
-                "",
-                Array(
-                    "AREA_FILE_SHOW" => "file",
-                    "AREA_FILE_SUFFIX" => "inc",
-                    "EDIT_TEMPLATE" => "",
-                    "PATH" => "/include/right.php"
-                )
-            );?>
+    </div>
+    <div class="row pt-5 mt-5 text-center">
+        <? $APPLICATION->IncludeComponent(
+            "bitrix:main.include",
+            "",
+            array(
+                "AREA_FILE_SHOW" => "file",
+                "AREA_FILE_SUFFIX" => "inc",
+                "EDIT_TEMPLATE" => "",
+                "PATH" => "/include/right.php"
+            )
+        ); ?>
 
-        </div>
+    </div>
 </footer>
 </body>
 </html>
