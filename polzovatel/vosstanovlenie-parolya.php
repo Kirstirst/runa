7a91176fc3d4ffbddaaf48c3500770c8c6e0ead6
Kirstirst <irinapanina2323@mail.ru>
2023-10-05 17:35:48 +0300

    task150365 part1-4

--- polzovatel/vosstanovlenie-parolya.php
+++ polzovatel/vosstanovlenie-parolya.php
@@ -2,10 +2,12 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Восстановление пароля");
 ?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.auth.forgotpasswd",
-	"",
-	Array(
+	"bitrix:main.auth.forgotpasswd", 
+	".default", 
+	array(
 		"AUTH_AUTH_URL" => "/polzovatel/auth.php",
-		"AUTH_REGISTER_URL" => "/polzovatel/registratsiya.php"
-	)
+		"AUTH_REGISTER_URL" => "/polzovatel/registratsiya.php",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
